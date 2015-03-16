58f97d5fbd74d2580145d6b850f0cbea90c6eff6
almas <almas.akchabayev@gmail.com>
2015-03-16 18:12:04 +0600

    IO Streams added

--- src/main/java/com/epam/aa/parser/Runner.java
+++ src/main/java/com/epam/aa/parser/Runner.java
@@ -3,20 +3,54 @@ package com.epam.aa.parser;
 import com.epam.aa.parser.model.Composite;
 import com.thedeanda.lorem.Lorem;
 
-import java.util.regex.Pattern;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Path;
+import java.util.Arrays;
 
 public class Runner {
     public static void main(String[] args) {
-        String text = Lorem.getParagraphs(3,5);
+        String text = Lorem.getParagraphs(3, 5);
+        byte[] bytes = text.getBytes();
 
-        Composite textComposite = Composite.parse(text);
-//        textComposite.print();
-//        System.out.println(textComposite);
+        OutputStream out;
+        try {
+            out = new BufferedOutputStream(new FileOutputStream("src/main/resources/output.txt"));
+            out.write(bytes);
+            out.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Reader reader;
+        String textFromFile = null;
+        try {
+            reader = new BufferedReader(new FileReader("src/main/resources/input.txt"));
+            StringBuffer sb = new StringBuffer();
+            int i;
+            while ((i = reader.read()) != -1) {
+                sb.append((char) i);
+            }
+            textFromFile = sb.toString();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(textFromFile);
+        System.out.println("");
+        System.out.println("");
+
+
+        Composite textComposite = Composite.parse(textFromFile);
         System.out.println(textComposite.toString());
-        System.out.printf("");
+        System.out.println("");
+        System.out.println("");
         System.out.println(textComposite.stringify());
-        System.out.printf("");
-        System.out.printf(text);
-//        Pattern
+        System.out.println("");
+        System.out.println("");
+        System.out.println(text);
     }
 }
