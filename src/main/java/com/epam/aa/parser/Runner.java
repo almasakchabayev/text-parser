473b94bbf0436f84e4fc7e4e4b92dafdf9166188
almas <almas.akchabayev@gmail.com>
2015-03-16 15:24:11 +0600

    added Lorem Ipsum lib and random text

--- src/main/java/com/epam/aa/parser/Runner.java
+++ src/main/java/com/epam/aa/parser/Runner.java
@@ -1,28 +1,22 @@
 package com.epam.aa.parser;
 
 import com.epam.aa.parser.model.Composite;
+import com.thedeanda.lorem.Lorem;
 
 import java.util.regex.Pattern;
 
 public class Runner {
     public static void main(String[] args) {
-        String text = "Licensed to the Apache Software Foundation , under one or more" +
-                "  contributor license agreements...  See the NOTICE file distributed with" +
-                "  this work for additional information regarding copyright ownership." +
-                "  The ASF licenses this file to You under the Apache License, Version 2.0; " +
-                "  you may not use this file except in compliance with" +
-                "  the License.\n" +
-                "  Another paragraph \n" +
-                "  Unless required by applicable law or agreed to in writing, software" +
-                "  distributed under the License is distributed on an AS IS BASIS," +
-                "  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied." +
-                "  See the License for the specific language governing permissions and" +
-                "  limitations under the License.";
+        String text = Lorem.getParagraphs(3,5);
 
         Composite textComposite = Composite.parse(text);
 //        textComposite.print();
 //        System.out.println(textComposite);
+        System.out.println(textComposite.toString());
+        System.out.printf("");
         System.out.println(textComposite.stringify());
+        System.out.printf("");
+        System.out.printf(text);
 //        Pattern
     }
 }
